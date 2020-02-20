2fc255427fa09653cdaf4cc833c17344220c0c21
dAnders0n <57796632+dAnders0n@users.noreply.github.com>
2020-02-20 14:05:48 +0100

    v0.96
    First commit.
    
    Image is bit broken.

---
+++ fancy_mission_start.VR/dialogs.hpp
@@ -0,0 +1,80 @@
+class dAn_Intro_Dialog
+{
+	idd = 9001;
+	movingEnbled = false;
+	
+	class controls
+	{
+		////////////////////////////////////////////////////////
+		// GUI EDITOR OUTPUT START (by D.Anderson, v1.063, #Tyqyfu)
+		////////////////////////////////////////////////////////
+
+		class dAn_rsc_Picture: RscPicture
+		{
+			idc = 1200;
+			text = "";
+			x = 0.448437 * safezoneW + safezoneX;
+			y = 0.247 * safezoneH + safezoneY;
+			w = 0.103125 * safezoneW;
+			h = 0.198 * safezoneH;
+		};
+		class dAn_rscStructuredText_1: RscStructuredText
+		{
+			idc = 1100;
+			x = 0.365938 * safezoneW + safezoneX;
+			y = 0.467 * safezoneH + safezoneY;
+			w = 0.268125 * safezoneW;
+			h = 0.044 * safezoneH;
+			colorBackground[] = {0,0,0,1};
+		};
+		class dAn_rscStructuredText_2: RscStructuredText
+		{
+			idc = 1101;
+			x = 0.376249 * safezoneW + safezoneX;
+			y = 0.533 * safezoneH + safezoneY;
+			w = 0.118594 * safezoneW;
+			h = 0.022 * safezoneH;
+			colorBackground[] = {0,0,0,1};
+		};
+		class dAn_rscStructuredText_3: RscStructuredText
+		{
+			idc = 1102;
+			x = 0.505156 * safezoneW + safezoneX;
+			y = 0.533 * safezoneH + safezoneY;
+			w = 0.118594 * safezoneW;
+			h = 0.022 * safezoneH;
+			colorBackground[] = {0,0,0,1};
+		};
+		class dAn_rscStructuredText_4: RscStructuredText
+		{
+			idc = 1103;
+			x = 0.37625 * safezoneW + safezoneX;
+			y = 0.566 * safezoneH + safezoneY;
+			w = 0.118594 * safezoneW;
+			h = 0.022 * safezoneH;
+			colorBackground[] = {0,0,0,1};
+		};
+		class dAn_rscStructuredText_5: RscStructuredText
+		{
+			idc = 1104;
+			x = 0.505156 * safezoneW + safezoneX;
+			y = 0.566 * safezoneH + safezoneY;
+			w = 0.118594 * safezoneW;
+			h = 0.022 * safezoneH;
+			colorBackground[] = {0,0,0,1};
+		};
+		class dAn_rscStructuredText_6: RscStructuredText
+		{
+			idc = 1105;
+			x = 0.376249 * safezoneW + safezoneX;
+			y = 0.599 * safezoneH + safezoneY;
+			w = 0.2475 * safezoneW;
+			h = 0.022 * safezoneH;
+			colorBackground[] = {0,0,0,1};
+		};
+		////////////////////////////////////////////////////////
+		// GUI EDITOR OUTPUT END
+		////////////////////////////////////////////////////////
+	};
+	
+};
